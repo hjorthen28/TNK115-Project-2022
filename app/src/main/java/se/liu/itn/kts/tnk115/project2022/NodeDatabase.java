2602eda54e8e0c74922db663d4e788be5bbeeffd
S.Hjorth <78598778+hjorthen28@users.noreply.github.com>
2022-06-08 18:48:31 +0200

    Delete app directory

--- app/src/main/java/se/liu/itn/kts/tnk115/project2022/NodeDatabase.java
+++
@@ -1,9 +0,0 @@
-package se.liu.itn.kts.tnk115.project2022;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Node.class}, version = 1)
-public abstract class NodeDatabase extends RoomDatabase {
-    public abstract NodeDao userDao();
-}
